69a7ceb09cddd91028cf87dd4012005bc554cbb6
vinhjaxt <ngoc.lo.dang@gmail.com>
2019-12-06 17:32:09 +0700

    Update new-website.sh

--- new-website.sh
+++ new-website.sh
@@ -3,6 +3,7 @@ read -p "Enter website name or domain (unique): " localdomainname
 cp internal-php.local internal-${localdomainname} -r
 cp nginx-proxy/conf.d/php.local.conf nginx-proxy/conf.d/${localdomainname}.conf
 mkdir public_html/${localdomainname}
+chmod 777 public_html/${localdomainname}
 echo 'Hello world, it works!' > public_html/${localdomainname}/index.html
 sed -i "s/php.local/${localdomainname}/g" internal-${localdomainname}/.env nginx-proxy/conf.d/${localdomainname}.conf
 nano nginx-proxy/conf.d/${localdomainname}.conf
