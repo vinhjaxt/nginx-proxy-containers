513ad9e775661c43103486d5af7342291c812158
vinhjaxt <ngoc.lo.dang@gmail.com>
2019-12-06 17:31:36 +0700

    Create centos-install-php-composer.sh

---
+++ centos-install-php-composer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum --enablerepo=remi-php73 install -y php
+yum --enablerepo=remi-php73 install -y php-xml php-soap php-xmlrpc php-mbstring php-json php-gd php-mcrypt
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+echo -ne "\nalias composer='php $(pwd)/composer.phar'\n" >>  ~/.bashrc
+source ~/.bashrc
