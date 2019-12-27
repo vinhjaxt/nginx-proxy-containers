fddaf0984286df395c1b2ffd97ac7de8a24cdb4b
ngoc.lo.dang@gmail.com <ngoc.lo.dang@gmail.com>
2019-12-27 14:49:51 +0700

    update :tada:

--- xmysql-create-db-and-user.sh
+++ xmysql-create-db-and-user.sh
@@ -2,9 +2,10 @@
 read -p "mysql root password: " rootpwd
 read -p "database name: " dbname
 read -p "database password: " dbpass
-docker exec -it internal-mysql.local mysql -uroot -p"${rootpwd}" -e "CREATE USER '${dbname}'@'localhost' IDENTIFIED BY '${dbpass}';"
-docker exec -it internal-mysql.local mysql -uroot -p"${rootpwd}" -e "CREATE USER '${dbname}'@'%' IDENTIFIED BY '${dbpass}';"
-docker exec -it internal-mysql.local mysql -uroot -p"${rootpwd}" -e "CREATE DATABASE \`${dbname}\`;"
-docker exec -it internal-mysql.local mysql -uroot -p"${rootpwd}" -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbname}'@'localhost';"
-docker exec -it internal-mysql.local mysql -uroot -p"${rootpwd}" -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbname}'@'%';"
-docker exec -it internal-mysql.local mysql -uroot -p"${rootpwd}" -e "FLUSH PRIVILEGES;"
+command="docker exec -it internal-mysql.local sh -c 'mysql -uroot -p"\""${rootpwd}"\"" -e "
+sh -c "$command "\""CREATE USER '\\''${dbname}'\\''@'\\''localhost'\\'' IDENTIFIED BY '\\''${dbpass}'\\'';"\""'"
+sh -c "$command "\""CREATE USER '\\''${dbname}'\\''@'\\''%'\\'' IDENTIFIED BY '\\''${dbpass}'\\'';"\""'"
+sh -c "$command "\""CREATE DATABASE \\\`${dbname}\\\`;"\""'"
+sh -c "$command "\""GRANT ALL PRIVILEGES ON \\\`${dbname}\\\`.* TO '\\''${dbname}'\\''@'\''localhost'\\'';"\""'"
+sh -c "$command "\""GRANT ALL PRIVILEGES ON \\\`${dbname}\\\`.* TO '\\''${dbname}'\\''@'\\''%'\\'';"\""'"
+sh -c "$command "\""FLUSH PRIVILEGES;"\""'"
